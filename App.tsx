8224e7dbab4c1cba4052c36cdd095dd12b3f3a53
DamianYongjae <klick214@gmail.com>
2020-04-29 14:26:46 -0400

    preloading cache

--- App.tsx
+++ App.tsx
@@ -3,10 +3,16 @@ import { Ionicons } from "@expo/vector-icons";
 import { AppLoading } from "expo";
 import * as Font from "expo-font";
 import { Asset } from "expo-asset";
-import { Text, View } from "react-native";
+import { Text, View, AsyncStorage } from "react-native";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo-hooks";
+import apolloClientOptions from "./apollo";
 
 export default function App() {
   const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
   const preLoad = async () => {
     try {
       await Font.loadAsync({
@@ -16,7 +22,17 @@ export default function App() {
         require("./assets/logo.svg"),
         require("./assets/logo_text.png"),
       ]);
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage,
+      });
+      const client = new ApolloClient({
+        cache,
+        ...apolloClientOptions,
+      });
       setLoaded(true);
+      setClient(client);
     } catch (e) {
       console.log(e);
     }
@@ -24,10 +40,12 @@ export default function App() {
   useEffect(() => {
     preLoad();
   }, []);
-  return loaded ? (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+  return loaded && client ? (
+    <ApolloProvider client={client}>
+      <View>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </ApolloProvider>
   ) : (
     <AppLoading />
   );
